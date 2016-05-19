1ce1b5a3ae0b238435d1f204c75c3f0119413468
Martin Sandström <maartiin_91@hotmail.com>
2016-05-19 13:46:14 +0200

    Adding basic calculation example

--- basic-calc.cbl
+++ basic-calc.cbl
@@ -1,16 +1,16 @@
       * run cobc -x test.cbl to compile.
       * run ./test to run the compile program.
 				Identification Division.
-		    Program-ID. sampleCOBOL.
+				Program-ID. sampleCOBOL.
 
 
-		    Data Division.
+				Data Division.
 				WORKING-STORAGE SECTION.
-        01  NUM1      PIC 9(1) VALUE 0.
+				01  NUM1      PIC 9(1) VALUE 0.
 
-		    Procedure Division.
-		    Main-Paragraph.
+				Procedure Division.
+				Main-Paragraph.
 						COMPUTE NUM1 = 1 + 2
 						Display "Hello World!"
 						Display NUM1
-		        Stop Run.
+						Stop Run.
