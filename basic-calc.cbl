8029649b5659a067faa44fa85a770e208d74f09a
Martin Sandström <maartiin_91@hotmail.com>
2016-05-19 14:21:18 +0200

    Adding functionality for basic calculation

--- basic-calc.cbl
+++ basic-calc.cbl
@@ -8,13 +8,14 @@
 				WORKING-STORAGE SECTION.
 				01  Num1      PIC 9 VALUE 0.
 				01  Num2      PIC 9 VALUE 0.
-				01  Result    PIC 9 VALUE 0.
+				01  Result    PIC 99 VALUE 0.
 
 				PROCEDURE DIVISION.
 			  		DISPLAY "Enter first number"
 						ACCEPT Num1.
+						ADD Num1 TO Result.
     				DISPLAY "Enter next number"
 						ACCEPT Num2
-						MULTIPLY Num1 BY Num2 GIVING Result.
-						DISPLAY Result
+						ADD Num2 TO Result.
+						DISPLAY Result.
 		        Stop Run.
