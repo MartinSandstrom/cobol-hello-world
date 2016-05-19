97735a524c3edfbccb434cbb72d68ed59f400a3f
Martin Sandström <maartiin_91@hotmail.com>
2016-05-19 13:19:18 +0200

    first commit

---
+++ test.cbl
@@ -0,0 +1,11 @@
+      * run cobc -x test.cbl to compile.
+      * run ./test to run the compile program.
+				Identification Division.
+		    Program-ID. sampleCOBOL.
+
+		    Data Division.
+
+		    Procedure Division.
+		    Main-Paragraph.
+		        Display "Hello World!"
+		        Stop Run.
